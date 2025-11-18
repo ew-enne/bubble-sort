f4e125e5a110d1cbc4146784514e7289d9ab037b
Ewenne <221577967+ew-enne@users.noreply.github.com>
2025-11-18 09:44:47 +0100

    Add comments to test if algorythm correct

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,19 +1,33 @@
-# test_arr = [2, 1, 3]
-test_arr = [2, 4, 6, 5, 1, 9, 3, 8]
+# test_arr = [1, 2, 3]
+test_arr = [2, 1, 3]
+# test_arr = [2, 4, 6, 5, 1, 9, 3, 8]
 
 def bubble_sort(arr)
-  # one passage of the array
+  
+  counter = 0 # counts number of changes
+  passage = 0 # counts number of passages
+
+  # make one full passage of the array
   arr.each_with_index do |element, index|
-    if index < (arr.length - 1)
+    if index < (arr.length - passage - 1)
       first_element = arr[index]
       second_element = arr[index + 1]
       if first_element > second_element
         arr[index] = second_element
         arr[index + 1] = first_element
+        counter += 1
       end
       p arr
     end
   end
+  puts "counter = #{counter}"
+  if counter == 0
+    puts "--> nothing to change"
+    # return
+  end
+  passage += 1
+  puts "passage = #{passage}"
+  puts "---> array after #{passage} passage:"
   p arr
 end
 
