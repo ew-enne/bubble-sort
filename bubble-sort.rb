b1fb33c058d186271cb67dc96c07f3e4869e3a01
Ewenne <221577967+ew-enne@users.noreply.github.com>
2025-11-18 10:12:29 +0100

    Add for loop
    This finished the method.

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,34 +1,40 @@
+# --- arrays for testing method ---
 # test_arr = [1, 2, 3]
-test_arr = [2, 1, 3]
+# test_arr = [2, 1, 3]
 # test_arr = [2, 4, 6, 5, 1, 9, 3, 8]
+# test_arr = [4,3,78,2,0,2]
+
 
 def bubble_sort(arr)
   
-  counter = 0 # counts number of changes
   passage = 0 # counts number of passages
+  limit = arr.length - 1 # max nummber of passages
 
-  # make one full passage of the array
-  arr.each_with_index do |element, index|
-    if index < (arr.length - passage - 1)
-      first_element = arr[index]
-      second_element = arr[index + 1]
-      if first_element > second_element
-        arr[index] = second_element
-        arr[index + 1] = first_element
-        counter += 1
+  # make (max) limit number of passages
+  for i in 1..(limit)
+
+    # make one full passage of the array
+    counter = 0 # counts number of changes
+
+    arr.each_with_index do |element, index|
+      if index < (limit - passage)
+        first_element = arr[index]
+        second_element = arr[index + 1]
+        if first_element > second_element
+          arr[index] = second_element
+          arr[index + 1] = first_element
+          counter += 1
+        end
       end
-      p arr
     end
+
+    if counter == 0 # array is in the right order
+      p arr
+      return
+    end
+    passage += 1
+
   end
-  puts "counter = #{counter}"
-  if counter == 0
-    puts "--> nothing to change"
-    # return
-  end
-  passage += 1
-  puts "passage = #{passage}"
-  puts "---> array after #{passage} passage:"
-  p arr
 end
 
 bubble_sort(test_arr)
